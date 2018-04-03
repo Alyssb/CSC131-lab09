5934daa5aae5cce0708117a2c0aa965e9ced60e6
Alyssb <ajs41@missouristate.edu>
2018-04-03 13:14:05 -0500

    final version, no comments

--- lab09.cpp
+++ lab09.cpp
@@ -14,19 +14,23 @@ bool isMagic(std::string someDate);
 
 int main()
 {
-    std::cout << "enter a date like this: dd/mm/yy pls & ty <3 ";
+    std::cout << "enter a date like this: dd/mm/yy pls & ty ";
     std::string ogDate{};
-    std::getline(std::cin, ogDate);
-    std::cout << "is the date " << ogDate << " magic???? ";
+    getline(std::cin, ogDate);
+    std::cout << "is the date "
+              << ogDate
+              << " magic???? ";
     if(isMagic(ogDate) == true)
-        std::cout << "Yes" << std::endl;
+        std::cout << "yeah dude!"
+                  << std::endl;
     else
-        std::cout << "no, sry" << std::endl;
+        std::cout << "no, sry"
+                  << std::endl;
 
 }
 
 bool isMagic(std::string someDate) {
-    std::cout << someDate << std::endl;
+    bool magic = false;
     int month{};
     int day{};
     int year{};
@@ -35,7 +39,9 @@ bool isMagic(std::string someDate) {
     day = std::stoi(someDate.substr(3,4));
     year = std::stoi(someDate.substr(6.7));
 
-    std::cout << month << " " << day << " " << year << std::endl;
+    if(month*day == year) {
+        magic = true;
+    }
 
-    return false;
+    return magic;
 }
