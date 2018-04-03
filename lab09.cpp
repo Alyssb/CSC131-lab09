47a4d3284c5d59ed9fd06d45f1f64121dc9ff995
Alyssa Slayton <ajs41@missouristate.edu>
2018-04-03 12:41:33 -0500

    updated version in cmake

--- lab09.cpp
+++ lab09.cpp
@@ -1,10 +1,13 @@
 /**
- * The date June l0, 1960 is special because when we write it in the following format, the month times the day equals
+ * The date June l0, 1960 is special because when we write it in the following format,
+ * the month times the day equals
  * the year.
  *
  * 6/10/60
  *
- * Write a program that asks the user to enter a month (in numeric form), a day, and a two-digit year. The program should then determine whether the month times the day is equal to the year.
+ * Write a program that asks the user to enter a month (in numeric form), a day, and a
+ * two-digit year. The program should then determine whether the month times the day is
+ * equal to the year.
  */
 
 // TODO: Add any additional libraries needed to compile your project.
@@ -14,7 +17,8 @@ bool isMagic(std::string someDate);
 
 int main()
 {
-    // TODO: Write a program that asks the user to enter a month (in numeric form), a day, and a two-digit year.
+    // TODO: Write a program that asks the user to enter a month (in numeric form), a day,
+    // and a two-digit year.
 
     // TODO: Call isMagic to determine
 }
