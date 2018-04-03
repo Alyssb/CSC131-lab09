c2c8008d91ca790be76d12aaf4c305f12809dc26
Alyssa Slayton <ajs41@missouristate.edu>
2018-04-03 13:24:00 -0500

    final version with comments and reformatting

--- lab09.cpp
+++ lab09.cpp
@@ -8,10 +8,18 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <string>
 
+/**
+ * takes an inputted date and calculates whether or not the date is magic (month * day = year)
+ * @param someDate date as inputted by user in format mm/dd/yy
+ * @return boolean value whether date meets requirements
+ */
 bool isMagic(std::string someDate);
 
+/**
+ * entry point of this application
+ * @return EXIT_SUCCESS is returned upon successful execution of this application
+ */
 int main()
 {
     std::cout << "enter a date like this: dd/mm/yy pls & ty ";
@@ -20,26 +28,28 @@ int main()
     std::cout << "is the date "
               << ogDate
               << " magic???? ";
-    if(isMagic(ogDate) == true)
+    if (isMagic(ogDate))
         std::cout << "yeah dude!"
                   << std::endl;
     else
         std::cout << "no, sry"
                   << std::endl;
 
+    return EXIT_SUCCESS;
 }
 
-bool isMagic(std::string someDate) {
+bool isMagic(std::string someDate)
+{
     bool magic = false;
     int month{};
     int day{};
     int year{};
 
-    month = std::stoi(someDate.substr(0,2));
-    day = std::stoi(someDate.substr(3,4));
-    year = std::stoi(someDate.substr(6.7));
+    month = std::stoi(someDate.substr(0, 2));
+    day = std::stoi(someDate.substr(3, 4));
+    year = std::stoi(someDate.substr(6, 7));
 
-    if(month*day == year) {
+    if (month*day==year) {
         magic = true;
     }
 
