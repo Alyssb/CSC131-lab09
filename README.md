a20cdd0a95cee428a6c37bb64358aabb69483f1f
Mr. Daehn <jdaehn@missouristate.edu>
2018-04-03 11:45:03 -0500

    LAB09-MASTER - Initial import.

---
+++ README.md
@@ -0,0 +1,18 @@
+The date June l0, 1960 is special because when we write it in the following format, the month times the day equals the year.
+
+6/10/60
+
+Write a program that asks the user to enter a month (in numeric form), a day, and a two-digit year. The program should then determine whether the month times the day is equal to the year.
+
+Below are some sample executions of this program:
+
+```text
+Enter a date in the form dd/mm/yy: 06/03/18
+Is the date 06/03/18 magic? Yes
+```
+
+```text
+Enter a date in the form dd/mm/yy: 07/03/18
+Is the date 07/03/18 magic? No
+```
+
---
+++ lab09.cpp
@@ -0,0 +1,26 @@
+/**
+ * The date June l0, 1960 is special because when we write it in the following format, the month times the day equals
+ * the year.
+ *
+ * 6/10/60
+ *
+ * Write a program that asks the user to enter a month (in numeric form), a day, and a two-digit year. The program should then determine whether the month times the day is equal to the year.
+ */
+
+// TODO: Add any additional libraries needed to compile your project.
+#include <string>
+
+bool isMagic(std::string someDate);
+
+int main()
+{
+    // TODO: Write a program that asks the user to enter a month (in numeric form), a day, and a two-digit year.
+
+    // TODO: Call isMagic to determine
+}
+
+bool isMagic(std::string someDate)
+{
+    // TODO: Implement me properly
+    return false;
+}
