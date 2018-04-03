e318771847f5ae9915fac7960efdfd4544ad6618
Alyssb <ajs41@missouristate.edu>
2018-04-03 13:02:23 -0500

    created month, day, and year integers

--- lab09.cpp
+++ lab09.cpp
@@ -1,13 +1,4 @@
 /**
- * The date June l0, 1960 is special because when we write it in the following format,
- * the month times the day equals
- * the year.
- *
- * 6/10/60
- *
- * Write a program that asks the user to enter a month (in numeric form), a day, and a
- * two-digit year. The program should then determine whether the month times the day is
- * equal to the year.
  * CSC131 - Computational Thinking
  * Missouri State University, Spring 2018
  *
@@ -23,12 +14,10 @@ bool isMagic(std::string someDate);
 
 int main()
 {
-    // TODO: Write a program that asks the user to enter a month (in numeric form), a day,
-    // and a two-digit year.
     std::cout << "enter a date like this: dd/mm/yy pls & ty <3 ";
     std::string ogDate{};
     std::getline(std::cin, ogDate);
-    std::cout << "is the date " << ogDate << "magic???? ";
+    std::cout << "is the date " << ogDate << " magic???? ";
     if(isMagic(ogDate) == true)
         std::cout << "Yes" << std::endl;
     else
@@ -36,6 +25,17 @@ int main()
 
 }
 
-bool isMagic(std::string date) {
+bool isMagic(std::string someDate) {
+    std::cout << someDate << std::endl;
+    int month{};
+    int day{};
+    int year{};
+
+    month = std::stoi(someDate.substr(0,2));
+    day = std::stoi(someDate.substr(3,4));
+    year = std::stoi(someDate.substr(6.7));
+
+    std::cout << month << " " << day << " " << year << std::endl;
+
     return false;
 }
